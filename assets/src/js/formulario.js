eed931bc813c25e59721809fe29ec2e1fa384249
rafaella.lopes <rafaella.lopes@idwall.co>
2021-06-10 02:56:33 -0300

    fix/new section adicionaPaciente

---
+++ assets/src/js/formulario.js
@@ -0,0 +1,9 @@
+function mostrarFormulario() {
+   const formulario = document.getElementById('formulario');
+   formulario.classList.add('mostrar');
+}
+
+function ocultarFormulario() {
+   const formulario = document.getElementById('formulario');
+   formulario.classList.add('invisivel');
+}

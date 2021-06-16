9ccb7507faffbadc91199db43aae3a7a546fddb4
rafaella.lopes <rafaella.lopes@idwall.co>
2021-06-16 00:32:36 -0300

    fix/button mostrarEOcultar

--- assets/src/js/formulario.js
+++
@@ -1,9 +0,0 @@
-function mostrarFormulario() {
-   const formulario = document.getElementById('formulario');
-   formulario.classList.add('mostrar');
-}
-
-function ocultarFormulario() {
-   const formulario = document.getElementById('formulario');
-   formulario.classList.add('invisivel');
-}
